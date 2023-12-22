0a743c0cb3c346b004919400a26b78290306dbe9
Admin <Admin@LAPCUABANCUTI>
2023-12-22 17:32:01 +0700

    Formdiem

---
+++ Form/Formdiem.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace PhanMemQuanLyDiemSinhVien
+{
+    partial class Formdiem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_timkiem = new System.Windows.Forms.Button();
+            this.cbx_monhoc = new System.Windows.Forms.ComboBox();
+            this.txb_timkiem = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbx_Hocky = new System.Windows.Forms.ComboBox();
+            this.ln_chonhocki = new System.Windows.Forms.Label();
+            this.dtgrv_bangdiemsv = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column13 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column14 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column15 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btn_Xuatfileex = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgrv_bangdiemsv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.btn_timkiem);
+            this.panel1.Controls.Add(this.cbx_monhoc);
+            this.panel1.Controls.Add(this.txb_timkiem);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.cbx_Hocky);
+            this.panel1.Controls.Add(this.ln_chonhocki);
+            this.panel1.Location = new System.Drawing.Point(49, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1257, 59);
+            this.panel1.TabIndex = 17;
+            // 
+            // btn_timkiem
+            // 
+            this.btn_timkiem.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_timkiem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_timkiem.Location = new System.Drawing.Point(3, 12);
+            this.btn_timkiem.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_timkiem.Name = "btn_timkiem";
+            this.btn_timkiem.Size = new System.Drawing.Size(96, 26);
+            this.btn_timkiem.TabIndex = 15;
+            this.btn_timkiem.Text = "Tìm kiếm";
+            this.btn_timkiem.UseVisualStyleBackColor = false;
+            // 
+            // cbx_monhoc
+            // 
+            this.cbx_monhoc.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbx_monhoc.FormattingEnabled = true;
+            this.cbx_monhoc.Items.AddRange(new object[] {
+            "Toán ",
+            "Lý ",
+            "Hóa ",
+            "Lập trình C++"});
+            this.cbx_monhoc.Location = new System.Drawing.Point(1087, 30);
+            this.cbx_monhoc.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cbx_monhoc.Name = "cbx_monhoc";
+            this.cbx_monhoc.Size = new System.Drawing.Size(121, 24);
+            this.cbx_monhoc.TabIndex = 14;
+            // 
+            // txb_timkiem
+            // 
+            this.txb_timkiem.Location = new System.Drawing.Point(105, 16);
+            this.txb_timkiem.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txb_timkiem.Name = "txb_timkiem";
+            this.txb_timkiem.Size = new System.Drawing.Size(231, 22);
+            this.txb_timkiem.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(986, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 23);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Môn học - ";
+            // 
+            // cbx_Hocky
+            // 
+            this.cbx_Hocky.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbx_Hocky.FormattingEnabled = true;
+            this.cbx_Hocky.Items.AddRange(new object[] {
+            "Học kì 1",
+            "Học kì 2",
+            "Học kì 3"});
+            this.cbx_Hocky.Location = new System.Drawing.Point(1087, 2);
+            this.cbx_Hocky.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cbx_Hocky.Name = "cbx_Hocky";
+            this.cbx_Hocky.Size = new System.Drawing.Size(121, 24);
+            this.cbx_Hocky.TabIndex = 8;
+            // 
+            // ln_chonhocki
+            // 
+            this.ln_chonhocki.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ln_chonhocki.AutoSize = true;
+            this.ln_chonhocki.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ln_chonhocki.Location = new System.Drawing.Point(986, 5);
+            this.ln_chonhocki.Name = "ln_chonhocki";
+            this.ln_chonhocki.Size = new System.Drawing.Size(89, 23);
+            this.ln_chonhocki.TabIndex = 10;
+            this.ln_chonhocki.Text = "Học Kỳ   - ";
+            // 
+            // dtgrv_bangdiemsv
+            // 
+            this.dtgrv_bangdiemsv.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dtgrv_bangdiemsv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgrv_bangdiemsv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9,
+            this.Column10,
+            this.Column11,
+            this.Column12,
+            this.Column13,
+            this.Column14,
+            this.Column15});
+            this.dtgrv_bangdiemsv.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.dtgrv_bangdiemsv.Location = new System.Drawing.Point(49, 78);
+            this.dtgrv_bangdiemsv.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dtgrv_bangdiemsv.Name = "dtgrv_bangdiemsv";
+            this.dtgrv_bangdiemsv.RowHeadersWidth = 51;
+            this.dtgrv_bangdiemsv.RowTemplate.Height = 24;
+            this.dtgrv_bangdiemsv.Size = new System.Drawing.Size(1257, 368);
+            this.dtgrv_bangdiemsv.TabIndex = 18;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "STT";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "Mã học phần";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column3.HeaderText = "Tên học phần";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column4.HeaderText = "Số tín chỉ";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column5.HeaderText = "Lần học";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column6.HeaderText = "Lần thi";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column7.HeaderText = "Đánh giá";
+            this.Column7.MinimumWidth = 6;
+            this.Column7.Name = "Column7";
+            // 
+            // Column8
+            // 
+            this.Column8.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column8.HeaderText = "Mã sinh viên";
+            this.Column8.MinimumWidth = 6;
+            this.Column8.Name = "Column8";
+            // 
+            // Column9
+            // 
+            this.Column9.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column9.HeaderText = "Chuyên cần";
+            this.Column9.MinimumWidth = 6;
+            this.Column9.Name = "Column9";
+            // 
+            // Column10
+            // 
+            this.Column10.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column10.HeaderText = "Điểm thực hành";
+            this.Column10.MinimumWidth = 6;
+            this.Column10.Name = "Column10";
+            // 
+            // Column11
+            // 
+            this.Column11.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column11.HeaderText = "Điểm thảo luận";
+            this.Column11.MinimumWidth = 6;
+            this.Column11.Name = "Column11";
+            // 
+            // Column12
+            // 
+            this.Column12.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column12.HeaderText = "Kiểm tra giữa kì";
+            this.Column12.MinimumWidth = 6;
+            this.Column12.Name = "Column12";
+            // 
+            // Column13
+            // 
+            this.Column13.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column13.HeaderText = "Thi kết thúc";
+            this.Column13.MinimumWidth = 6;
+            this.Column13.Name = "Column13";
+            // 
+            // Column14
+            // 
+            this.Column14.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column14.HeaderText = "Tổng kết học phần";
+            this.Column14.MinimumWidth = 6;
+            this.Column14.Name = "Column14";
+            // 
+            // Column15
+            // 
+            this.Column15.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column15.HeaderText = "Điểm chữ";
+            this.Column15.MinimumWidth = 6;
+            this.Column15.Name = "Column15";
+            // 
+            // btn_Xuatfileex
+            // 
+            this.btn_Xuatfileex.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_Xuatfileex.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Xuatfileex.Location = new System.Drawing.Point(1039, 460);
+            this.btn_Xuatfileex.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_Xuatfileex.Name = "btn_Xuatfileex";
+            this.btn_Xuatfileex.Size = new System.Drawing.Size(116, 41);
+            this.btn_Xuatfileex.TabIndex = 19;
+            this.btn_Xuatfileex.Text = "Xuất file exel";
+            this.btn_Xuatfileex.UseVisualStyleBackColor = false;
+            // 
+            // Formdiem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1354, 525);
+            this.Controls.Add(this.btn_Xuatfileex);
+            this.Controls.Add(this.dtgrv_bangdiemsv);
+            this.Controls.Add(this.panel1);
+            this.Name = "Formdiem";
+            this.Text = "Formdiem";
+            this.Load += new System.EventHandler(this.Formdiem_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgrv_bangdiemsv)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_timkiem;
+        private System.Windows.Forms.ComboBox cbx_monhoc;
+        private System.Windows.Forms.TextBox txb_timkiem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbx_Hocky;
+        private System.Windows.Forms.Label ln_chonhocki;
+        private System.Windows.Forms.DataGridView dtgrv_bangdiemsv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column12;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column13;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column14;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column15;
+        private System.Windows.Forms.Button btn_Xuatfileex;
+    }
+}
